ec594d8488a2907cc537ea2da628e05f828a2151
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-03-10 18:52:41 +0300

    initial commit

---
+++ src/test/java/com/example/japapp/JapAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.japapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
